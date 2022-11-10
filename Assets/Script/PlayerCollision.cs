c53231216d68cb74e18ff75ed59a43493c4e3b35
Dangthap <dangthithap9@gmail.com>
2022-11-10 16:32:35 +0700

    *GameOver

--- Assets/Script/PlayerCollision.cs
+++ Assets/Script/PlayerCollision.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     public PlayerCollision movement;
+    public GameManager gameManager;
 
     void OnCollisionEnter(Collision CollisionInfo)
     {
         if (CollisionInfo.collider.tag == "Obstacle")
         {
             movement.enabled = false;
+            FindObjectOftype<GameManager>().EndGame();
 
+        }
+        if (rb.position.y < -1f)
+        {
+            FindObjectOftype<GameManager>().endGame();
+            
         }
     }
     
