556ad27f530e9129d7357a79542dcb539e8b7e92
Dangthap <dangthithap9@gmail.com>
2022-11-10 09:42:38 +0700

    *Collision

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,17 @@
+
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerCollision movement;
+
+    void OnCollisionEnter(Collision CollisionInfo)
+    {
+        if (CollisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+
+        }
+    }
+    
+}
